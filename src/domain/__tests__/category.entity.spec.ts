9e94378283243c78bba01f25cac6ab760d9e3f49
evrwaq <evrwaq@gmail.com>
2024-07-27 02:25:21 +0000

    test: ensure Category is created with all values

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -2,7 +2,7 @@ import { Category } from '../category.entity';
 
 describe('Category Unit Tests', () => {
   describe('constructor', () => {
-    test('should create a category with default values', () => {
+    test('Should create a category with default values', () => {
       const category = new Category({
         name: 'Movie'
       });
@@ -11,6 +11,21 @@ describe('Category Unit Tests', () => {
       expect(category.description).toBeNull();
       expect(category.is_active).toBeTruthy();
       expect(category.created_at).toBeInstanceOf(Date);
+    })
+
+    test('Should create a category with all values', () => {
+      const created_at = new Date();
+      const category = new Category({
+        name: 'Movie',
+        description: 'Movie description',
+        is_active: false,
+        created_at
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe('Movie');
+      expect(category.description).toBe('Movie description');
+      expect(category.is_active).toBeFalsy();
+      expect(category.created_at).toBe(created_at);
     })
   })
 })
