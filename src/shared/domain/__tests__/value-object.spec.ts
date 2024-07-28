cd7b11f5d5a136bce3a69814f10d4489843296f8
evrwaq <evrwaq@gmail.com>
2024-07-28 20:33:56 +0000

    test: ensure ValueObject returns false on not equals

--- src/shared/domain/__tests__/value-object.spec.ts
+++ src/shared/domain/__tests__/value-object.spec.ts
@@ -21,4 +21,18 @@ describe('ValueObject Unit Tests', () => {
     const complexValueObject2 = new ComplexValueObject('test', 1);
     expect(complexValueObject1.equals(complexValueObject2)).toBe(true);
   })
+
+  test('Should not be equals', () => {
+    const valueObject1 = new StringValueObject('test');
+    const valueObject2 = new StringValueObject('test2');
+    expect(valueObject1.equals(valueObject2)).toBe(false);
+    expect(valueObject1.equals(null as any)).toBe(false);
+    expect(valueObject1.equals(undefined as any)).toBe(false);
+
+    const complexValueObject1 = new ComplexValueObject('test', 1);
+    const complexValueObject2 = new ComplexValueObject('test', 2);
+    expect(complexValueObject1.equals(complexValueObject2)).toBe(false);
+    expect(complexValueObject1.equals(null as any)).toBe(false);
+    expect(complexValueObject1.equals(undefined as any)).toBe(false);
+  })
 })
