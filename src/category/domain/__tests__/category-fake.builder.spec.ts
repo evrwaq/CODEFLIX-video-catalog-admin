ffcde758ee09bd572b6c05b23d7a7dec853534f0
evrwaq <evrwaq@gmail.com>
2024-12-24 22:13:05 +0000

    test: ensure CategoryFakeBuilder category_id is undefined

--- src/category/domain/__tests__/category-fake.builder.spec.ts
+++ src/category/domain/__tests__/category-fake.builder.spec.ts
@@ -2,7 +2,7 @@ import { Chance } from 'chance'
 import { CategoryFakeBuilder } from '../category-fake.builder'
 import { Uuid } from '../../../shared/domain/value-objects/uuid.vo'
 
-describe('CategoryFakeBuilder Unit Tests', () => {
+describe('CategoryFakerBuilder Unit Tests', () => {
   describe('category_id prop', () => {
     const faker = CategoryFakeBuilder.category()
 
@@ -10,6 +10,10 @@ describe('CategoryFakeBuilder Unit Tests', () => {
       expect(() => faker.category_id).toThrowError(
         new Error("Property category_id not have a factory, use 'with' methods")
       )
+    })
+
+    test('should be undefined', () => {
+      expect(faker['_category_id']).toBeUndefined()
     })
   })
 })
