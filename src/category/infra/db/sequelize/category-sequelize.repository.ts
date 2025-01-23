ff56dc76ce8ec4fceaf6844e4dfa7fd7e9b1ad50
Evrwaq <evrwaq@gmail.com>
2025-01-23 01:57:00 +0000

    feat: implement insert method

---
+++ src/category/infra/db/sequelize/category-sequelize.repository.ts
@@ -0,0 +1,50 @@
+import { Uuid } from '../../../../shared/domain/value-objects/uuid.vo'
+import { Category } from '../../../domain/category.entity'
+import { ICategoryRepository } from '../../../domain/category.repository'
+import { CategoryModel } from './category.model'
+
+export class CategorySequelizeRepository implements ICategoryRepository {
+  sortableFields: string[] = ['name', 'created_at']
+
+  constructor(private categoryModel: typeof CategoryModel) {}
+
+  async insert(entity: Category): Promise<void> {
+    await this.categoryModel.create({
+      category_id: entity.category_id.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at,
+    })
+  }
+
+  async bulkInsert(entities: Category[]): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async update(entity: Category): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async delete(category_id: Uuid): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findById(entity_id: Uuid): Promise<Category | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findAll(): Promise<Category[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async search(props: CategorySearchParams): Promise<CategorySearchResult> {
+    throw new Error('Method not implemented.')
+  }
+
+  private formatSort(sort: string, sort_dir: SortDirection) {}
+
+  getEntity(): new (...args: any[]) => Category {
+    throw new Error('Method not implemented.')
+  }
+}
