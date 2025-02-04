3e92a9b6a7f7168b8686abd70e8af54737d2f8aa
Evrwaq <evrwaq@gmail.com>
2025-02-04 00:54:55 +0000

    test: ensure CategorySequelizeRepository finds a category by id

--- src/category/infra/db/sequelize/__tests__/category-sequelize.repository.test.ts
+++ src/category/infra/db/sequelize/__tests__/category-sequelize.repository.test.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript'
 import { CategoryModel } from '../category.model'
 import { CategorySequelizeRepository } from '../category-sequelize.repository'
 import { Category } from '../../../../domain/category.entity'
+import { Uuid } from '../../../../../shared/domain/value-objects/uuid.vo'
 
 describe('CategorySequelizeRepository Integration Test', () => {
   let sequelize
@@ -23,5 +24,15 @@ describe('CategorySequelizeRepository Integration Test', () => {
     await repository.insert(category)
     const entity = await repository.findById(category.category_id)
     expect(entity.toJSON()).toStrictEqual(category.toJSON())
+  })
+
+  it('should find a category by id', async () => {
+    let categoryFound = await repository.findById(new Uuid())
+    expect(categoryFound).toBeNull()
+
+    const category = Category.fake().category().build()
+    await repository.insert(category)
+    categoryFound = await repository.findById(category.category_id)
+    expect(category.toJSON()).toStrictEqual(categoryFound.toJSON())
   })
 })
