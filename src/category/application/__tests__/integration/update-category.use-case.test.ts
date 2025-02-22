a29955197e1654163a641cc62c2ffd43e884cee1
Evrwaq <evrwaq@gmail.com>
2025-02-22 01:07:42 +0000

    test: ensure UpdateCategoryUseCase updates a category

--- src/category/application/__tests__/integration/update-category.use-case.test.ts
+++ src/category/application/__tests__/integration/update-category.use-case.test.ts
@@ -24,4 +24,133 @@ describe('UpdateCategoryUseCase Integration Tests', () => {
       useCase.execute({ id: uuid.id, name: 'fake' })
     ).rejects.toThrow(new NotFoundError(uuid.id, Category))
   })
+
+  it('should update a category', async () => {
+    const category = Category.fake().category().build()
+    repository.insert(category)
+
+    let output = await useCase.execute({
+      id: category.category_id.id,
+      name: 'test',
+    })
+
+    expect(output).toStrictEqual({
+      id: category.category_id.id,
+      name: 'test',
+      description: category.description,
+      is_active: true,
+      created_at: category.created_at,
+    })
+
+    type Arrange = {
+      input: {
+        id: string
+        name: string
+        description?: string | null
+        is_active?: boolean
+      }
+      expected: {
+        id: string
+        name: string
+        description: string | null
+        is_active: boolean
+        created_at: Date
+      }
+    }
+    const arrange: Arrange[] = [
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: true,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          is_active: false,
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: false,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: false,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          is_active: true,
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: true,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          is_active: false,
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: false,
+          created_at: category.created_at,
+        },
+      },
+    ]
+
+    for (const i of arrange) {
+      output = await useCase.execute({
+        id: i.input.id,
+        ...('name' in i.input && { name: i.input.name }),
+        ...('description' in i.input && { description: i.input.description }),
+        ...('is_active' in i.input && { is_active: i.input.is_active }),
+      })
+      const entityUpdated = await repository.findById(new Uuid(i.input.id))
+      expect(output).toStrictEqual({
+        id: category.category_id.id,
+        name: i.expected.name,
+        description: i.expected.description,
+        is_active: i.expected.is_active,
+        created_at: entityUpdated.created_at,
+      })
+      expect(entityUpdated.toJSON()).toStrictEqual({
+        category_id: category.category_id.id,
+        name: i.expected.name,
+        description: i.expected.description,
+        is_active: i.expected.is_active,
+        created_at: entityUpdated.created_at,
+      })
+    }
+  })
 })
