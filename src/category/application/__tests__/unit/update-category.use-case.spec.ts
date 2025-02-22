c7d34d971b88ae380cea8687aa3e7d0d431061a3
Evrwaq <evrwaq@gmail.com>
2025-02-22 00:27:54 +0000

    test: ensure UpdateCategoryUseCase updates a category

--- src/category/application/__tests__/unit/update-category.use-case.spec.ts
+++ src/category/application/__tests__/unit/update-category.use-case.spec.ts
@@ -27,4 +27,127 @@ describe('UpdateCategoryUseCase Unit Tests', () => {
       useCase.execute({ id: uuid.id, name: 'fake' })
     ).rejects.toThrow(new NotFoundError(uuid.id, Category))
   })
+
+  it('should update a category', async () => {
+    const spyUpdate = jest.spyOn(repository, 'update')
+    const category = new Category({ name: 'Movie' })
+    repository.items = [category]
+
+    let output = await useCase.execute({
+      id: category.category_id.id,
+      name: 'test',
+    })
+
+    expect(spyUpdate).toHaveBeenCalledTimes(1)
+    expect(output).toStrictEqual({
+      id: category.category_id.id,
+      name: 'test',
+      description: null,
+      is_active: true,
+      created_at: category.created_at,
+    })
+
+    type Arrange = {
+      input: {
+        id: string
+        name: string
+        description?: string | null
+        is_active?: boolean
+      }
+      expected: {
+        id: string
+        name: string
+        description: string | null
+        is_active: boolean
+        created_at: Date
+      }
+    }
+    const arrange: Arrange[] = [
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: true,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          is_active: false,
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: false,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: false,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          is_active: true,
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: true,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.category_id.id,
+          name: 'test',
+          is_active: false,
+        },
+        expected: {
+          id: category.category_id.id,
+          name: 'test',
+          description: 'some description',
+          is_active: false,
+          created_at: category.created_at,
+        },
+      },
+    ]
+
+    for (const i of arrange) {
+      output = await useCase.execute({
+        id: i.input.id,
+        ...('name' in i.input && { name: i.input.name }),
+        ...('description' in i.input && { description: i.input.description }),
+        ...('is_active' in i.input && { is_active: i.input.is_active }),
+      })
+      expect(output).toStrictEqual({
+        id: category.category_id.id,
+        name: i.expected.name,
+        description: i.expected.description,
+        is_active: i.expected.is_active,
+        created_at: i.expected.created_at,
+      })
+    }
+  })
 })
